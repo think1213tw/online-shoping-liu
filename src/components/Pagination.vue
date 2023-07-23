dbcac623eae61bf6bd97d7adc7f301117a0331ab
liu <think1213tw@gmail.com>
2023-07-23 16:28:12 +1000

    [切換上下一頁分頁功能]

--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -2,7 +2,15 @@
   <nav aria-label="Page navigation example">
     <ul class="pagination justify-content-center">
       <li class="page-item">
-        <a class="page-link" href="#" aria-label="Previous">
+        <a
+          class="page-link"
+          :class="{
+            disabled: !pages.has_pre
+          }"
+          href="#"
+          aria-label="Previous"
+          @click.prevent="previousPage(pages.current_page)"
+        >
           <span aria-hidden="true">&laquo;</span>
         </a>
       </li>
@@ -17,7 +25,15 @@
         }}</a>
       </li>
       <li class="page-item">
-        <a class="page-link" href="#" aria-label="Next">
+        <a
+          class="page-link"
+          href="#"
+          aria-label="Next"
+          :class="{
+            disabled: !pages.has_next
+          }"
+          @click.prevent="nextPage(pages.current_page)"
+        >
           <span aria-hidden="true">&raquo;</span>
         </a>
       </li>
@@ -30,10 +46,22 @@
 // @emitPages="更新頁面事件"
 export default {
   props: ['pages'],
+  data() {
+    return {
+      test: {}
+    }
+  },
   methods: {
     updatePage(page) {
       console.log(page)
       this.$emit('emit-pages', page)
+    },
+    nextPage(page) {
+      console.log('test', page)
+      this.$emit('emit-pages', page + 1)
+    },
+    previousPage(page) {
+      this.$emit('emit-pages', page - 1)
     }
   }
 }
